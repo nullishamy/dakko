200ec8f2bc6f1e258bfd5006742c3d19c1ec7707
nullishamy <git@amyerskine.me>
2024-06-21 00:20:31 +0100

    feat: start anew

--- flake.nix
+++ flake.nix
@@ -34,65 +34,20 @@
       devShells = forEachSystem (
         { pkgs, ... }:
         {
-          default =
-            let
-              libraries = with pkgs; [
-                webkitgtk
-                gtk3
-                cairo
-                gdk-pixbuf
-                glib
-                dbus
-                openssl_3
-                librsvg
-              ];
-
-              packages = with pkgs; [
-                curl
-                wget
-                pkg-config
-                dbus
-                openssl_3
-                glib
-                gtk3
-                libsoup
-                webkitgtk
-                librsvg
-              ];
-            in
-            pkgs.mkShell {
-              env = {
-                LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH";
-                XDG_DATA_DIRS = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS";
-                GIO_MODULE_DIR = "${pkgs.glib-networking}/lib/gio/modules/";
-              };
-
-              packages =
-                with pkgs;
-                [
-                  nodejs_20
-                  nodePackages.yarn
-                  (rust-bin.stable.latest.default.override {
-                    extensions = [
-                      "rust-src"
-                      "rust-analyzer"
-                      "clippy"
-                    ];
-                  })
-                ]
-                ++ packages;
-            };
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              (rust-bin.stable.latest.default.override {
+                extensions = [
+                  "rust-src"
+                  "rust-analyzer"
+                  "clippy"
+                ];
+              })
+            ];
+          };
         }
       );
 
       formatter = forEachSystem ({ pkgs, ... }: pkgs.nixfmt-rfc-style);
-
-      packages = forEachSystem (
-        { pkgs, system }:
-        {
-          dakko = pkgs.callPackage ./package.nix { };
-          default = self.packages.${system}.dakko;
-        }
-      );
     };
 }
